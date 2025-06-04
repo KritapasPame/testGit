1d348d05d5057e2af16369ac55f3461115a039b4
kamonnscx <127631239+kamonnscx@users.noreply.github.com>
2025-06-04 15:00:47 +0700

    addname new feature

--- tests/Feature/AddNameTest.php
+++ tests/Feature/AddNameTest.php
@@ -8,25 +8,7 @@ use App\Models\Name;
 
 class AddNameTest extends TestCase
 {
-    // public function test_add_name_successfully()
-    // {
 
-    //     $name = 'เนย1';
-
-
-    //     $response = $this->post('/addname', [
-    //         'name' => $name,
-    //     ]);
-
-    //     // ตรวจสอบว่ามีชื่อในฐานข้อมูลแล้ว
-    //     $this->assertDatabaseHas('names', [
-    //         'name' => $name,
-    //     ]);
-
-    //     // ตรวจสอบว่า redirect กลับหลัง submit
-    //     $response->assertRedirect();
-
-    // }
 
     public function test_add_name_validation_error()
     {
@@ -34,7 +16,7 @@ class AddNameTest extends TestCase
             'name' => '', // เว้นว่างเพื่อให้เกิด validation error
         ]);
 
-        // คาดว่าจะเกิด validation error
+        // คิดว่าจะเกิด validation error
         $response->assertSessionHasErrors('name');
     }
 }
