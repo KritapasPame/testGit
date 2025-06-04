b7f94bea19491b096ecdcb9287485294c9cdce75
kamonnscx <127631239+kamonnscx@users.noreply.github.com>
2025-06-04 15:15:39 +0700

    add newfeature addname

--- app/Http/Controllers/NameController.php
+++ app/Http/Controllers/NameController.php
@@ -16,7 +16,7 @@ class NameController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255|regex:/^[A-Za-z]+$/',
+            'name' => 'required|string|max:255',
         ]);
         Name::create([
             'name' => $request->input('name'),
